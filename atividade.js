9cbab12ae921cb00150f1375f5a2ee4921796c3b
Analia <analiavictoire@gmail.com>
2021-08-27 22:50:13 -0400

    Atividade 2

---
+++ atividade.js
@@ -0,0 +1,31 @@
+//Exercício 1
+//USE ALERT PARA MOSTRAR NA TELA UM "SEJA BEM VINDO AO MEU SITE"
+
+alert("Seja bem-vinda ao meu site!");
+
+//Exercício 2
+//USE ALERT PARA MOSTRAR O RESULTADO DA SOMA ENTRE 87 + 96
+
+function somatorio()
+{
+    let soma = 87 + 96;
+    alert("O resultado da soma entre 87 + 96 é igual a: " + soma);
+}
+
+//Exercício 3
+//USE ALERT PARA MOSTRAR O RESULTADO DA MULTIPLICACAO ENTRE 5 E 8
+
+function multiplicacao()
+{
+    let mult = 5 * 8;
+    alert("O resultado da multiplicação entre 5 x 8 é: " + mult);
+}
+
+//Exercício 4
+//CRIE UMA LISTA DE COMPRAS
+
+function listaDecompras()
+{
+    let lista = [' Café', ' Aveia', ' Arroz', ' Banana' ];
+    alert("Lista de compras: " + lista);
+}
